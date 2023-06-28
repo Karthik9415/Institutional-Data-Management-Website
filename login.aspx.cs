ba6fd2c46233b6dabe4019f83bb533dd108c20c3
karthik9415 <ganesankarthik.gk@gmail.com>
2023-06-28 21:33:51 +0530

    initial

---
+++ login.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using System.Collections;
+
+using System.Configuration;
+
+using System.Data;
+
+using System.Web.Security;
+
+
+using System.Web.UI.HtmlControls;
+
+
+using System.Web.UI.WebControls.WebParts;
+
+using System.Xml.Linq;
+
+using MySql.Data.MySqlClient;
+
+
+
+namespace website
+{
+    public partial class login : System.Web.UI.Page
+    {
+        MySqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["loginid"] != null)
+            {
+                Response.Redirect("home.aspx", true);
+            }
+
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.Now.AddDays(-1));
+            Response.Cache.SetNoStore();
+            con = new MySqlConnection("Data Source=localhost;Database=erp;User ID=root;Password= ");
+            con.Open();
+        }
+
+        protected void loginbutton_Click(object sender, EventArgs e)
+        {
+
+            string query = "select * from login where email=@loginid and password=@passwordid";
+            MySqlCommand sqlCmd = new MySqlCommand(query, con);
+            sqlCmd.Parameters.AddWithValue("@loginid",loginid.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@passwordid", passwordid.Text.Trim());
+            int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+            if (count == 1)
+            {               
+                Session["loginid"] = loginid.Text.Trim();
+                if (Session["loginid"] != null)
+                {
+                    Response.Redirect("home.aspx", true);
+                }
+                else
+                {                                                          
+                   ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "swal('SORRY','LOGIN FAILED','error')", true);
+                }
+            }
+            else {
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "swal('SORRY','LOGIN FAILED','error')", true);
+            }
+            con.Close();
+        }
+
+    }
+}
---
+++ master.Master.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace website
+{
+    public partial class master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.Now.AddDays(-1));
+            Response.Cache.SetNoStore();
+            if (Session["loginid"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
+
+        }
+        protected void logout(object sender, EventArgs e)
+        {
+            Session["loginid"] = null;
+            Session.Abandon();
+            Session.Clear();
+            Session.RemoveAll();
+            HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache,no-store,must-revalidate");
+            HttpContext.Current.Response.AddHeader("Pragma", "no-cache");
+            HttpContext.Current.Response.AddHeader("Expires","0");
+            Response.CacheControl = "no-cache";
+            if (Session["loginid"] == null) { 
+            Response.Redirect("login.aspx"); }
+        }
+    }
+}
